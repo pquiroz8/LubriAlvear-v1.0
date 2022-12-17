7beb1e577fa00aa1efdd88aa63c97ef06d0daf97
Pablo Quiróz <pquiroz.17@gmail.com>
2022-12-17 16:51:09 -0300

    Modificacion de la logica para agregar productos al carrito. Agregacion mensaje con los productos que se han agregado al finalizar la carga de productos

--- js/realizarCompra.js
+++ js/realizarCompra.js
@@ -1,6 +1,6 @@
 //!Harcodeo los productos cargados porque no puedo traerme los valores cargados en cargarStock() hasta que no veamos storage
 
-const productos_catalogo = [
+const productosCatalogo = [
     {prodId:1, prodName:"Aceite Motul 5w30", prodCategory:"Lubricantes", prodPrice: 5140.00, prodStock:300},
     {prodId:2, prodName:"Aceite Mannol 5w40", prodCategory:"Lubricantes", prodPrice:8935.00, prodStock:200},
     {prodId:3, prodName:"Caja Fusilera Peugeot 408", prodCategory:"Electricidad", prodPrice:62000.00, prodStock:20},
@@ -22,31 +22,34 @@ function realizarCompra(){
 //*Cargamos el carrito con los productos elegidos
     while (cargaCarrito) {
         let prodDisponibles = "";
-        for (let item of productos_catalogo){
+        for (let item of productosCatalogo){
             prodDisponibles += "Id: " + item.prodId + " - " + item.prodName + "    $ " + item.prodPrice + "    Stock: " + item.prodStock +"\n"; 
         }
     
         productoEnCarrito = parseInt(prompt("Seleccione un producto para agregar al carrito:(Ingrese su Id)\n" + prodDisponibles));
         console.log("Id ingresado: " + productoEnCarrito);
         prodAgregado = buscarProducto(productoEnCarrito);
-        console.log(prodAgregado);
-
+            
         if (prodAgregado != null){
+            console.log(prodAgregado);
             carritoCompra.push(prodAgregado);
-        } else {
-            alert("Usted ingresó: " + productoEnCarrito + ".\n" + "Ningún producto se agregará a su carrito.");
+            alert("Se ha agregado el producto con Id: " + productoEnCarrito);
+            console.log(carritoCompra);
         }
-        console.log(carritoCompra);
         cargaCarrito = confirm("¿Desea agregar otro producto?");
     }
-    
     const carritoCompraUnicos = eliminarDuplicados(carritoCompra);
     console.log(carritoCompraUnicos);
-}
 
+    let prodCargados = "";
+        for (let item of carritoCompraUnicos){
+            prodCargados += "Id: " + item.prodId + " - " + item.prodName + "    $ " + item.prodPrice + "    Stock: " + item.prodStock +"\n"; 
+        }
+    alert("SE HAN CARGADO LOS SIGUIENTES PRODUCTOS: \n" + prodCargados);
+}
 //* Busco el producto con el Id ingresado, devuelvo null si ingresa un Id que no está registrado
 function buscarProducto(id){
-    return (productos_catalogo.find(prod => prod.prodId === id) || null);
+    return (productosCatalogo.find(prod => prod.prodId === id) || null);
 }
 
 //* Función para eliminar elementos duplicados. De esta manera solo se mostrará una vez el producto ingresado
