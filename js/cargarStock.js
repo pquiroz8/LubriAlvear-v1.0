fee7547e6304ad094d82ed429af508acaf38f42b
Pablo Quiróz <pquiroz.17@gmail.com>
2023-01-12 18:40:37 -0300

    Se modificó el enlace a Sweet Alert

--- js/cargarStock.js
+++ js/cargarStock.js
@@ -31,7 +31,6 @@ const cargarNuevoProducto = () => {
 document.getElementById("btn-agregarProd").addEventListener("click",cargarNuevoProducto);
 
 
-
 function validarCarga () {
     let prodName = document.getElementById("prodName").value;
     let prodUnits = document.getElementById("prodUnits").value;
@@ -39,7 +38,6 @@ function validarCarga () {
 
     (prodName.trim() == '') ? mostrarError("Nombre del producto") : (prodUnits == 0 ) ? mostrarError("Unidades") : (prodPrice == 0) ? mostrarError("Precio") : false
 }
-
 
 function mostrarError(campo) {
     Swal.fire({
@@ -50,46 +48,3 @@ function mostrarError(campo) {
         color: '#FCFFE7',
         confirmButtonText: '¡Entendido!'})
 }
-
-
-    /* //* Carga de productos
-    while (cargaProd) { ///@audit-ok 
-            do{ //Validación para la categoría del producto
-                prodCategory = prompt("Ingrese la categoría del producto(Lubricantes/Electricidad/Otros)").toUpperCase();
-                console.log("Categoría: " + prodCategory);
-            } while ((prodCategory != "LUBRICANTES") && (prodCategory != "ELECTRICIDAD") && (prodCategory != "OTROS"));
-            
-
-        do {    
-            do{ //Validación para nombre distinto de vacío
-                prodName = prompt("Ingrese el nombre del producto").toUpperCase();
-                console.log("Producto: " + prodName);
-            } while (prodName == null);
-
-            do{ //Validación stock > 0
-                prodStock = parseInt(prompt("Ingrese la cantidad de productos a registrar"));
-                console.log("Stock: " + prodStock);
-            } while (prodStock < 0); 
-
-            do{ // Validación precio > 0
-                prodPrice = parseFloat(prompt("Ingrese el precio del producto"));
-                console.log("Precio: " + prodPrice);
-            } while (prodPrice < 0 );
-
-            prodId = productosStock.length + 1;
-            producto = new Producto(prodId,prodCategory,prodName,prodStock,prodPrice); 
-            console.log(producto);
-            productosStock.push(producto);
-            console.log(productosStock);
-            sameCategory = confirm("Desea agregar otro producto de la misma categoría");
-        } while (sameCategory);
-        prodId = productosStock.length + 1;
-        cargaProd = confirm("¿Desea agregar un nuevo producto?"); 
-    }
-
-    let prodCargados = "";
-        for (let item of productosStock){
-            prodCargados += "Id: " + item.prodId + " - " + item.prodName + "    $ " + item.prodPrice + "    Stock: " + item.prodStock +"\n"; 
-        }
-    alert("SE HAN CARGADO LOS SIGUIENTES PRODUCTOS: \n" + prodCargados);
-}*/
